cf6c464de436825cf6e7bc94224a68a32cd6558a
ifrag <piskunov.alesha@gmail.com>
2019-10-23 11:29:09 +0700

    urls in templates

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,15 @@
 import pathlib
 import os
 
-from backend.views import index
+from backend.views import index, content
 
 
 PROJECT_ROOT = pathlib.Path(os.getenv('PYTHONPATH', __file__))
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_get('/', index, name='index')
+    app.router.add_get('/content', content, name='content')
     setup_static_routes(app)
 
 
